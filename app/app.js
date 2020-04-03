a7275a1a4e599188e97216beee356fe148e3a6be
BhupinderS24 <ikwvibe1@gmail.com>
2020-04-04 01:13:37 +0530

    second commit

--- app/app.js
+++ app/app.js
@@ -3,15 +3,29 @@ const app = express();
 const logger = require('./logger/logger.js');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://bhupinder:' +process.env.MONGO_ATLAS_PW + '@friends-2d4ln.mongodb.net/test?retryWrites=true&w=majority',
-{useNewUrlParser: true});
+
+var dev_db_url= 'mongodb+srv://bhupinder:bhupinder@friends-2d4ln.mongodb.net/test?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+mongoose.connect(mongoDB, {useNewUrlParser: true});
 
 
 app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-
+app.use((req,res,next)=>{
+    res.header('Access-control-Allow-Origin','*');
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+    );
+    if(req.method==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+})
 app.use('/friendsapp', require('./routes/admin.js'));
 
 const PORT = process.env.PORT || 3000;
